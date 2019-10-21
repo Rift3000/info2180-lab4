9d868d6cbef330a6d9b6f0f1c8ef2b45402eb808
Jason Williams <williamsjacen007@gmail.com>
2019-10-20 22:30:42 -0500

    Fully working js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,103 +1,118 @@
 
-document.write("Why you not working?");
- 
 window.onload = function (){
-	/*var t = document.querySelectorAll("div");
-	var i = t[3].setAttribute("class", "square");
-	var ii = t[4].setAttribute("class", "square");
-	var iii = t[5].setAttribute("class", "square");
-	var iv = t[6].setAttribute("class", "square");
-	var v = t[7].setAttribute("class", "square");
-	var vi = t[8].setAttribute("class", "square");
-	var vii = t[9].setAttribute("class", "square");
-	var viii = t[10].setAttribute("class", "square");
-	var ix = t[11].setAttribute("class", "square");
 
 
-	var up = document.getElementById("zen");
-
-	var up = document.getElementById("zen");
-	up.addEventListener("mouseover", Ham);
-	
-	function Ham () {
-		up.style.backgroundColor = "red";
-	};
-
-	ix.addEventListener("mouseover", Gamer); 
-	function Gamer () {
-		ix.style.backgroundColor = "blue";
-	};*/
-	/*var boxes = document.getElementById("board");*/
-
-
-	var boxes = document.getElementById("board").querySelectorAll("div");
+	var boxes = document.getElementById("board").querySelectorAll("div"), option = 0;
 	var i;
 	for (i = 0; i < boxes.length; i++) {
 		boxes[i].setAttribute("class", "square");
 	}
 
-	
-	var track = [ "#", '#', "#", "#", "#", "#", "#", "#", "#"];
-	var box0 = boxes[0];
-    
-    box0.addEventListener("click", Gamer); 
-	function Gamer () {
-		if ( box0 == null ) {
-			box0.innerHTML = "X";
-			box0.classList.add("square", "square.X");
-		
-		}else{
-			console.log('Please select another square');
-		}
-		
-	};
-	
-	
-	/*
-	boxes.onclick = function() {
-		boxes.setAttribute("class", "square.X");
-	};*/
+	var check = new Array();
 
+	function winners(){
+
+	var box1 = boxes[0];
+	var box2 = boxes[1];
+	var box3 = boxes[2];
+	var box4 = boxes[3];
+	var box5 = boxes[4];
+	var box6 = boxes[5];
+	var box7 = boxes[6];
+	var box8 = boxes[7];
+	var box9 = boxes[8];
+
+	if(box1.innerHTML !== "" && box1.innerHTML === box2.innerHTML && box1.innerHTML === box3.innerHTML ){
+		var last = check[check.length - 2];	
+		var win = document.getElementById("status").innerHTML = "Congratulations! " + last + " is the Winner!";
+		win.className = "status.you-won";
+
+
+	} else if (box4.innerHTML !== "" && box4.innerHTML === box5.innerHTML && box4.innerHTML === box6.innerHTML){
+		var last = check[check.length - 2];	
+		var win = document.getElementById("status").innerHTML = "Congratulations! " + last + " is the Winner!";
+		win.className = "status.you-won";
+		
+
+	} else if (box7.innerHTML !== "" && box7.innerHTML === box8.innerHTML && box7.innerHTML === box9.innerHTML){
+		var last = check[check.length - 2];	
+		var win = document.getElementById("status").innerHTML = "Congratulations! " + last + " is the Winner!";
+		win.className = "status.you-won";
+		
+
+	} else if (box1.innerHTML !== "" && box1.innerHTML === box4.innerHTML && box1.innerHTML === box7.innerHTML){
+		var last = check[check.length - 2];	
+		var win = document.getElementById("status").innerHTML = "Congratulations! " + last + " is the Winner!";
+		win.className = "status.you-won";
+		
+
+	} else if (box2.innerHTML !== "" && box2.innerHTML === box5.innerHTML && box2.innerHTML === box8.innerHTML){
+		var last = check[check.length - 2];	
+		var win = document.getElementById("status").innerHTML = "Congratulations! " + last + " is the Winner!";
+		win.className = "status.you-won";
+		
+
+	} else if (box3.innerHTML !== "" && box3.innerHTML === box6.innerHTML && box3.innerHTML === box9.innerHTML){
+		var last = check[check.length - 2];	
+		var win = document.getElementById("status").innerHTML = "Congratulations! " + last + " is the Winner!";
+		win.className = "status.you-won";
+		
+
+	}else if (box1.innerHTML !== "" && box1.innerHTML === box5.innerHTML && box1.innerHTML === box9.innerHTML){
+		var last = check[check.length - 2];	
+		var win = document.getElementById("status").innerHTML = "Congratulations! " + last + " is the Winner!";
+		win.className = "status.you-won";
+
+	}else if (box3.innerHTML !== "" && box3.innerHTML === box5.innerHTML && box3.innerHTML === box7.innerHTML){
+		var last = check[check.length - 2];	
+		var win = document.getElementById("status").innerHTML = "Congratulations! " + last + " is the Winner!";
+		win.className = "status.you-won";
+
+	}
+
+	}
+	
+
+	for (var m = 0; m < boxes.length; m++){
+		boxes[m].onclick = function() {
+			if (this.innerHTML !== "X" && this.innerHTML !== "O"){
+			if(option%2 === 0){
+
+				this.innerHTML = "X";
+				winners();
+				option += 1;
+				this.classList.add(".square.X");
+				check.push("X");
+				//console.log(check);
+
+			}else {
+
+				this.innerHTML = "O";
+				winners();
+				option += 1;
+				this.classList.add(".square.O");
+				check.push("O");
+				//console.log(check);
+			}
+
+		};
+	}
+		boxes[m].addEventListener("mouseover", function (){
+				this.classList.add("hover");
+			});
+		
+		boxes[m].addEventListener("mouseout", function (){
+			this.setAttribute("class", "square");
+		});
+
+	}
+
+	var res = document.querySelector("button");
+
+	res.onclick = function () {
+	window.location.reload();
+	//console.log(res);
+	}
+	
 }
-
-
-document.write("Why you not working?");
-
-
-/*window.onload = function (){
-	var x = document.getElementById("board");
-	x.setAttribute("class", "square");
-
-};*/
-
-
-/*var w= document.getElementById("status");
-	w.style.backgroundColor = "yellow";
-var x = document.getElementById("board");
-
-	x.style.backgroundColor = "green";
-	var c = document.getElementById("board").childNodes.innerHTML = "Walk away now"; 
-document.body.style.backgroundColor = "red";*/
-
-/*alert("Run away while you can");*/
-
-/*document.addEventListener('DOMContentLoaded', ()
-document.getElementsByTagName("div").setAttribute("class", "square");*/
-
-/*document.getElementsByTagName("div").classList.add("square");
-
-
-var fixC = document.getElementById("board").childNodes;*/
-
-/*
-window.onload = function() {
-	return fixC.classList.add("square");
-};*/
-
-/*
-getElementById("try").style.height = 100;
-getElementById("try").style.width = 100;
-getElementById("try").style.backgroundColor = 'black';*/
-
-
 
